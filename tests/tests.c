a1cc32c9ff0df77eb002b0411c0b4d9f856dd178
Cugerbol <cugerbol@student.21-school.ru>
2024-04-20 19:30:32 +0300

    test coverage 80+

--- tests/tests.c
+++ tests/tests.c
@@ -12,6 +12,69 @@
 
 
 
+START_TEST(write_read_test)
+{
+int score_start = readDataBase();
+int score = score_start + 200;
+writeDataBase(score);
+int score_new = readDataBase();
+ck_assert_int_eq(score_new , score);
+
+
+}
+END_TEST
+
+START_TEST(levelUp_test)
+{
+TetGame_t * game = initGame();
+game->gameInfo->speed = 1;
+game->gameInfo->score= 600;
+levelUp(game);
+int new_speed = 2;
+ck_assert_int_eq(game->gameInfo->speed , new_speed);
+
+
+}
+END_TEST
+
+START_TEST(converterScore_1_row)
+{
+int count_rows = 1;
+int score = converterScore(count_rows);
+ck_assert_int_eq(score, 100);
+
+}
+END_TEST
+
+START_TEST(converterScore_2_row)
+{
+int count_rows = 2;
+int score = converterScore(count_rows);
+ck_assert_int_eq(score, 300);
+
+}
+END_TEST
+
+START_TEST(converterScore_3_row)
+{
+int count_rows = 3;
+int score = converterScore(count_rows);
+ck_assert_int_eq(score, 700);
+
+}
+END_TEST
+
+START_TEST(converterScore_4_row)
+{
+int count_rows = 4;
+int score = converterScore(count_rows);
+ck_assert_int_eq(score, 1500);
+
+
+
+}
+END_TEST
+
 
 START_TEST(calculateTet_test_counterIter_is_full)
 {
@@ -134,6 +197,46 @@ int sumBLocks = blocks[1][2] + blocks[2][1] +
             blocks[2][2] + blocks[3][2];
 ck_assert_int_eq(sumBLocks , 4);
 freeGame(game);
+
+
+}
+END_TEST
+
+START_TEST(calculateTet_test_Down_success_1)
+{
+TetGame_t * game = initGame();
+free(game->figure);
+game->figure = createRandomFigure(2);
+game->figure->y= HEIGHT_FIELD - 3;
+game->action = Down;
+calculateTet(game);
+int ** blocks = game->field->blocks;
+int sumBLocks = 0;
+for (int i = 0; i < WIDTH_FIELD; i++){
+    sumBLocks += blocks[HEIGHT_FIELD - 1][i];
+}
+ck_assert_int_eq(sumBLocks , 4);
+freeGame(game);
+
+
+}
+END_TEST
+
+START_TEST(calculateTet_test_Down_success_2)
+{
+TetGame_t * game = initGame();
+free(game->figure);
+game->figure = createRandomFigure(2);
+game->figure->y= HEIGHT_FIELD - 3;
+game->counterIter = COUNT_ITERATION * 15 / game->gameInfo->speed;
+calculateTet(game);
+int ** blocks = game->field->blocks;
+int sumBLocks = 0;
+for (int i = 0; i < WIDTH_FIELD; i++){
+    sumBLocks += blocks[HEIGHT_FIELD - 1][i];
+}
+ck_assert_int_eq(sumBLocks , 4);
+freeGame(game);
 }
 END_TEST
 
@@ -148,15 +251,41 @@ END_TEST
 
 START_TEST(createFigure_test_1)
 {
-
 TetFigure_t * figure = createRandomFigure(0);
 ck_assert_ptr_nonnull(figure);
-
 freeFigure(figure);
 
 
+}
+END_TEST
+
+START_TEST(updateFigure_test_1)
+{
+TetGame_t * game = initGame();
+updateFigure(game);
+int y_start = -5;
+ck_assert_ptr_nonnull(game->figure);
+ck_assert_int_eq(game->figure->y , y_start);
+freeGame(game);
 
 
+}
+END_TEST
+
+START_TEST(placeFigure_test_1)
+{
+TetGame_t * game = initGame();
+freeFigure(game->figure);
+game->figure = createRandomFigure(2);
+game->figure->y = HEIGHT_FIELD - 3;
+placeFigure(game);
+int sumBLocks = 0;
+for (int i = 0; i < WIDTH_FIELD; i++){
+    
+    sumBLocks += game->field->blocks[HEIGHT_FIELD - 1][i];
+}
+ck_assert_int_eq(sumBLocks, 4);
+freeGame(game);
 }
 END_TEST
 
@@ -167,7 +296,16 @@ TetGame_t * game = createGame();
 ck_assert_ptr_nonnull(game);
 freeGame(game);
 
+}
+END_TEST
 
+START_TEST(updateCurrentState_test_1)
+{
+    TetGame_t * game = initGame();
+    TetGame_t * game_new =  updateCurrentState(game);
+    ck_assert_ptr_nonnull(game);
+
+    freeGame(game);
 }
 END_TEST
 
@@ -381,60 +519,75 @@ END_TEST
 
 int main(void)
 {
-    Suite *s1 = suite_create("calculateTet");
-    TCase *tc1_1 = tcase_create("calculateTet");
-    Suite *s2 = suite_create("field");
-    TCase *tc2_1 = tcase_create("field");
-    Suite *s3 = suite_create("figure");
-    TCase *tc3_1 = tcase_create("figure");
-    Suite *s4 = suite_create("game");
-    TCase *tc4_1 = tcase_create("game");
-    Suite *s5 = suite_create("moveFigure");
-    TCase *tc5_1 = tcase_create("moveFigure");
-    Suite *s6 = suite_create("userAction");
-    TCase *tc6_1 = tcase_create("userAction");
+    Suite *s1 = suite_create("bonus");
+    TCase *tc1_1 = tcase_create("bonus");
+    Suite *s2 = suite_create("calculateTet");
+    TCase *tc2_1 = tcase_create("calculateTet");
+    Suite *s3 = suite_create("field");
+    TCase *tc3_1 = tcase_create("field");
+    Suite *s4 = suite_create("figure");
+    TCase *tc4_1 = tcase_create("figure");
+    Suite *s5 = suite_create("game");
+    TCase *tc5_1 = tcase_create("game");
+    Suite *s6 = suite_create("moveFigure");
+    TCase *tc6_1 = tcase_create("moveFigure");
+    Suite *s7 = suite_create("userAction");
+    TCase *tc7_1 = tcase_create("userAction");
     SRunner *sr = srunner_create(s1);
     int nf;
 
     suite_add_tcase(s1, tc1_1);
-    tcase_add_test(tc1_1, calculateTet_test_counterIter_is_full);
-    tcase_add_test(tc1_1, calculateTet_test_Right_success_1);
-    tcase_add_test(tc1_1, calculateTet_test_Right_success_2);
-    tcase_add_test(tc1_1, calculateTet_test_Right_unsuccess_1);
-    tcase_add_test(tc1_1, calculateTet_test_Left_success_1);
-    tcase_add_test(tc1_1, calculateTet_test_Left_success_2);
-    tcase_add_test(tc1_1, calculateTet_test_Left_unsuccess_1);
-    tcase_add_test(tc1_1, calculateTet_test_Up_success_1);
+    tcase_add_test(tc1_1, write_read_test);
+    tcase_add_test(tc1_1, levelUp_test);
+    tcase_add_test(tc1_1, converterScore_1_row);
+    tcase_add_test(tc1_1, converterScore_2_row);
+    tcase_add_test(tc1_1, converterScore_3_row);
+    tcase_add_test(tc1_1, converterScore_4_row);
     suite_add_tcase(s2, tc2_1);
-    tcase_add_test(tc2_1, createField_test_1);
+    tcase_add_test(tc2_1, calculateTet_test_counterIter_is_full);
+    tcase_add_test(tc2_1, calculateTet_test_Right_success_1);
+    tcase_add_test(tc2_1, calculateTet_test_Right_success_2);
+    tcase_add_test(tc2_1, calculateTet_test_Right_unsuccess_1);
+    tcase_add_test(tc2_1, calculateTet_test_Left_success_1);
+    tcase_add_test(tc2_1, calculateTet_test_Left_success_2);
+    tcase_add_test(tc2_1, calculateTet_test_Left_unsuccess_1);
+    tcase_add_test(tc2_1, calculateTet_test_Up_success_1);
+    tcase_add_test(tc2_1, calculateTet_test_Down_success_1);
+    tcase_add_test(tc2_1, calculateTet_test_Down_success_2);
     suite_add_tcase(s3, tc3_1);
-    tcase_add_test(tc3_1, createFigure_test_1);
+    tcase_add_test(tc3_1, createField_test_1);
     suite_add_tcase(s4, tc4_1);
-    tcase_add_test(tc4_1, createGame_test_1);
+    tcase_add_test(tc4_1, createFigure_test_1);
+    tcase_add_test(tc4_1, updateFigure_test_1);
+    tcase_add_test(tc4_1, placeFigure_test_1);
     suite_add_tcase(s5, tc5_1);
-    tcase_add_test(tc5_1, moveDown_test_succes);
-    tcase_add_test(tc5_1, moveLeft_test_succes);
-    tcase_add_test(tc5_1, moveRight_test_succes);
-    tcase_add_test(tc5_1, moveUp_test_succes);
+    tcase_add_test(tc5_1, createGame_test_1);
+    tcase_add_test(tc5_1, updateCurrentState_test_1);
     suite_add_tcase(s6, tc6_1);
-    tcase_add_test(tc6_1, userAction_test_KEY_UP);
-    tcase_add_test(tc6_1, userAction_test_KEY_DOWN);
-    tcase_add_test(tc6_1, userAction_test_KEY_LEFT);
-    tcase_add_test(tc6_1, userAction_test_KEY_RIGHT);
-    tcase_add_test(tc6_1, userAction_test_q);
-    tcase_add_test(tc6_1, userAction_test_p_1);
-    tcase_add_test(tc6_1, userAction_test_p_2);
-    tcase_add_test(tc6_1, userAction_test_increase_speed_1);
-    tcase_add_test(tc6_1, userAction_test_increase_speed_2);
-    tcase_add_test(tc6_1, userAction_test_reduce_speed_1);
-    tcase_add_test(tc6_1, userAction_test_reduce_speed_2);
-    tcase_add_test(tc6_1, userAction_test_reduce_speed_3);
+    tcase_add_test(tc6_1, moveDown_test_succes);
+    tcase_add_test(tc6_1, moveLeft_test_succes);
+    tcase_add_test(tc6_1, moveRight_test_succes);
+    tcase_add_test(tc6_1, moveUp_test_succes);
+    suite_add_tcase(s7, tc7_1);
+    tcase_add_test(tc7_1, userAction_test_KEY_UP);
+    tcase_add_test(tc7_1, userAction_test_KEY_DOWN);
+    tcase_add_test(tc7_1, userAction_test_KEY_LEFT);
+    tcase_add_test(tc7_1, userAction_test_KEY_RIGHT);
+    tcase_add_test(tc7_1, userAction_test_q);
+    tcase_add_test(tc7_1, userAction_test_p_1);
+    tcase_add_test(tc7_1, userAction_test_p_2);
+    tcase_add_test(tc7_1, userAction_test_increase_speed_1);
+    tcase_add_test(tc7_1, userAction_test_increase_speed_2);
+    tcase_add_test(tc7_1, userAction_test_reduce_speed_1);
+    tcase_add_test(tc7_1, userAction_test_reduce_speed_2);
+    tcase_add_test(tc7_1, userAction_test_reduce_speed_3);
 
     srunner_add_suite(sr, s2);
     srunner_add_suite(sr, s3);
     srunner_add_suite(sr, s4);
     srunner_add_suite(sr, s5);
     srunner_add_suite(sr, s6);
+    srunner_add_suite(sr, s7);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
